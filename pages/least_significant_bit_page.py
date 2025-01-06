f6a42a262c9137ebc3bebfac860b30357d7d4b1c
Anjasfedo <fedoaf98@gmail.com>
2025-01-06 17:36:52 +0700

    feat: handle max capacity of cover image

--- pages/least_significant_bit_page.py
+++ pages/least_significant_bit_page.py
@@ -22,44 +22,64 @@ if "output_file_name" not in st.session_state:
     st.session_state.output_file_name = None
 
 if option == "Embed Message":
-        st.header("Embed a Message into an Image")
+    st.header("Embed a Message into an Image")
 
-        # Upload the input image
-        uploaded_image = st.file_uploader(
-            "Upload an Image", type=["png", "jpg", "jpeg"])
-        if uploaded_image:
-            st.image(uploaded_image, caption="Uploaded Image Preview",
-                     use_column_width=True)
+    # Upload the input image
+    uploaded_image = st.file_uploader(
+        "Upload an Image", type=["png", "jpg", "jpeg"])
+    if uploaded_image:
+        input_image = Image.open(uploaded_image)
+        st.image(uploaded_image, caption="Uploaded Image Preview",
+                 use_column_width=True)
+
+        # Calculate maximum message length
+        img_width, img_height = input_image.size
+        # E.g., RGB has 3 channels
+        color_channels = len(input_image.getbands())
+        max_message_length = ((img_width * img_height *
+                              color_channels * k_val) - 1) // 8  # Max in characters, -1 of delimiter
+
+        st.info(f"Maximum Message Length: {max_message_length} characters")
 
         # Input the message to embed
-        message = st.text_area("Enter the Message to Embed")
-        st.info(
-            f"Message Length: {len(message)} characters")
+        message = st.text_area(
+            "Enter the Message to Embed",
+            max_chars=max_message_length,
+            placeholder=f"Max {max_message_length} characters",
+        )
+        st.info(f"Message Length: {len(message)} characters")
 
-        if uploaded_image and message:
+        if len(message) > max_message_length:
+            st.error("Message exceeds the maximum length!")
+
+        if uploaded_image and 0 < len(message) <= max_message_length:
             output_file_name = st.text_input(
-                "Output File Name", value="stego_image")
+                "Output File Name", value="stego_image"
+            )
 
             # Ensure the file has the `.png` extension
-            output_file_name = f"{output_file_name}.png" if not output_file_name.endswith(
-                ".png") else output_file_name
+            output_file_name = (
+                f"{output_file_name}.png"
+                if not output_file_name.endswith(".png")
+                else output_file_name
+            )
 
             if st.button("Embed Message"):
                 try:
-                    input_image = Image.open(uploaded_image)
                     # Save uploaded image for processing
                     input_image.save("temp_input_image.png")
 
                     # Embed the message
                     stego_image = lsb.embed_message(
-                        "temp_input_image.png", output_file_name, message)
+                        "temp_input_image.png", output_file_name, message
+                    )
 
-                    st.session_state.embed_done = True  # Mark embedding as done
                     st.session_state.stego_image = stego_image  # Store the stego image
                     st.session_state.output_file_name = output_file_name  # Store output file name
 
                     st.success(
-                        f"Message successfully embedded! Stego image saved as {output_file_name}")
+                        f"Message successfully embedded! Stego image saved as {output_file_name}"
+                    )
 
                     # Immediately show the stego image preview and download button
                     st.image(stego_image, caption="Stego Image Preview",
@@ -79,30 +99,6 @@ if option == "Embed Message":
                     )
                 except Exception as e:
                     st.error(f"An error occurred: {e}")
-    # else:
-    #     # Display the stego image preview and download button
-    #     stego_image = st.session_state.stego_image
-    #     st.image(stego_image, caption="Stego Image Preview",
-    #              use_column_width=True)
-
-    #     # Prepare the image for download
-    #     buffer = io.BytesIO()
-    #     stego_image.save(buffer, format="PNG")
-    #     buffer.seek(0)  # Reset the pointer to the start of the buffer
-
-    #     st.download_button(
-    #         label="Download Stego Image",
-    #         data=buffer.getvalue(),  # Get the binary data for download
-    #         file_name=st.session_state.output_file_name,
-    #         mime="image/png",
-    #     )
-
-    #     # Option to start over
-    #     if st.button("Start Over"):
-    #         st.session_state.embed_done = False
-    #         st.session_state.stego_image = None
-    #         st.session_state.output_file_name = None
-    #         st.rerun()
 
 elif option == "Extract Message":
     st.header("Extract a Message from an Image")
@@ -114,7 +110,6 @@ elif option == "Extract Message":
         st.image(stego_image, caption="Uploaded Stego Image Preview",
                  use_column_width=True)
 
-    if stego_image:
         if st.button("Extract Message"):
             try:
                 uploaded_stego_image = Image.open(stego_image)
@@ -125,8 +120,7 @@ elif option == "Extract Message":
                 extracted_message = lsb.extract_message("temp_stego_image.png")
 
                 st.success("Message successfully extracted!")
-                st.text_area("Extracted Message",
-                             extracted_message, height=200)
+                st.code(extracted_message, wrap_lines=True)
                 st.info(
                     f"Extracted Message Length: {len(extracted_message)} characters")
             except Exception as e:
